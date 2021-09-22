9a162345c353cae93413cc568b593a0584f4f895
kyoya <kyoyaprog@gmail.com>
2021-09-23 00:01:18 +0900

    first commit

---
+++ shareDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shareDiary
+//
+//  Created by 松山響也 on 2021/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
